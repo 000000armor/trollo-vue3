d2a4c7948f9e078ad4835e2469716f803f0898c6
David_Sevikian <david_sevikian@epam.com>
2022-05-10 00:55:52 +0300

    extract drag'n'drop logic into components

---
+++ src/components/AppDrag.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    draggable="true"
+    @dragover.prevent
+    @dragenter.prevent
+    @dragstart.self="handleDrag"
+  >
+    <slot />
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  transferData: Object,
+});
+
+const handleDrag = (event) => {
+  event.dataTransfer.effectAllowed = "move";
+  event.dataTransfer.dropEffect = "move";
+  event.dataTransfer.setData("payload", JSON.stringify(props.transferData));
+};
+</script>
---
+++ src/components/AppDrop.vue
@@ -0,0 +1,18 @@
+<template>
+  <div @drop.stop="handleDrop" @dragover.prevent @dragenter.prevent>
+    <slot />
+  </div>
+</template>
+
+<script setup>
+import { defineEmits } from "vue";
+
+const emit = defineEmits(["drop"]);
+const handleDrop = (event) => {
+  const transferData = JSON.parse(event.dataTransfer.getData("payload"));
+
+  emit("drop", transferData);
+};
+</script>
+
+<style scoped></style>
